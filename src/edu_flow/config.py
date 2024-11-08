5983edc10440bbac14582febb7dc8d4116252101
mberman84 <mberman84@gmail.com>
2024-11-08 09:06:30 -0800

    easier to switch between models

--- src/edu_flow/config.py
+++ src/edu_flow/config.py
@@ -1,12 +1,20 @@
 import os
 
-# Configuration for LLM
+# LLM_CONFIG = {
+#     "model": "gpt-4o-mini",
+#     "api_key": os.getenv('OPENAI_API_KEY')
+# }
+
 LLM_CONFIG = {
-    "model": "gpt-4o-mini",
-    "api_key": os.getenv('OPENAI_API_KEY')
+    "model": "groq/llama3-groq-70b-8192-tool-use-preview",
+    "api_key": os.getenv('GROQ_API_KEY')
 }
 
-# Configuration for EduFlow
+# LLM_CONFIG = {
+#     "model": "anthropic/claude-3-5-sonnet-20240620",
+#     "api_key": os.getenv('ANTHROPIC_API_KEY')
+# }
+
 EDU_FLOW_INPUT_VARIABLES = {
     "audience_level": "beginner",
     "topic": "Generative AI"
